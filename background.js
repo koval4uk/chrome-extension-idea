22809a7ddee91a86a695a48c593fb7ef4a19fd62
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-08-17 18:59:04 +0300

    Simplify plugin where plugin work

--- background.js
+++ background.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// todo work extension only on hostEquals: 'www.codewars.com'
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'www.codewars.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       console.log(sender.tab ?

1534f08a0e8f7a90263b4f783d501343f1119b24
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-08-17 20:07:21 +0300

    Remove redundant

--- README.md
+++ README.md
@@ -2,15 +2,3 @@
 1. https://developer.chrome.com/extensions/getstarted
 1. http://www.adambarth.com/experimental/crx/docs/getstarted.html
 1. https://github.com/learn-anything/chrome-extensions
-
-
-### Debug devtools.js:
-1. Open web page
-1. Open Chrome Dev Tools (F12)
-1. Open "MyPanel24" tab
-1. Open Chrome Dev Tools in "MyPanel24" 
-
-
-### Debug background.js:
-1. Extension setting 
-1. Click on Extension background page
--- devtools.js
+++ devtools.js
@@ -1,1 +1,35 @@
-chrome.devtools.panels.create("MyPanel24", null, 'panel.html');
+/**
+ * https://stackoverflow.com/questions/29845765/chrome-extension-using-devtool-for-har-file-save
+ * The chrome.devtools.* API modules
+ * are available only to the pages loaded within the DevTools window.
+ * Content scripts and other extension pages do not have these APIs.
+ * Thus, the APIs are available only through the lifetime of the DevTools window.
+ */
+chrome.devtools.network.onRequestFinished.addListener(request => {
+  let searchString = "/java/session";
+  if ( request.request.url.endsWith(searchString)) {
+    request.getContent((body) => {
+      console.log("intercept body = " + body);
+      sendRequest();
+    });
+  }
+});
+
+function sendRequest() {
+  // todo add jQuery for post request
+  let xhr = new XMLHttpRequest();
+// todo server https://webhook.site/#!/046cccae-578c-4776-ae45-1aa382c45dbd/0b788c0f-ec16-4cd4-9280-5c17de0fbfcf/1
+  xhr.open('GET', 'https://webhook.site/046cccae-578c-4776-ae45-1aa382c45dbd', false);
+
+  try {
+    xhr.send();
+    if (xhr.status !== 200) {
+      console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      console.log(xhr.response);
+    }
+  }
+  catch (err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
+    console.log("Запрос не удался");
+  }
+}

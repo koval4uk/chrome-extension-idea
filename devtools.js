5f17982c01669fa871865b6a0862d06efd6eb0f3
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-08-17 22:23:08 +0300

    Add header - Content-Type

--- devtools.js
+++ devtools.js
@@ -4,22 +4,26 @@
  * only to the pages loaded within the DevTools window.
  */
 chrome.devtools.network.onRequestFinished.addListener(request => {
-  let searchString = "/java/session";
-  if ( request.request.url.endsWith(searchString)) {
+  let urlSuffix = "/java/session";
+  if (request.request.url.endsWith(urlSuffix)) {
     request.getContent((body) => {
-      console.log("intercept body = " + body);
+      console.log("Handle body: " + body);
       sendRequest(body);
     });
   }
 });
 
 const url = "http://127.0.0.1:63350/api.keymapSwitcher";
+
 // const url = "https://webhook.site/046cccae-578c-4776-ae45-1aa382c45dbd";
 
 function sendRequest(body) {
   fetch(url, {
     method: "POST",
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    },
   }).then(res => {
     console.log("Request complete! response: ", res);
   });

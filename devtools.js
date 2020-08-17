854dbf5ea68553467cf9fafe371c1f7619f84e76
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-08-17 20:22:25 +0300

    Add comment

--- devtools.js
+++ devtools.js
@@ -1,9 +1,7 @@
 /**
- * https://stackoverflow.com/questions/29845765/chrome-extension-using-devtool-for-har-file-save
- * The chrome.devtools.* API modules
- * are available only to the pages loaded within the DevTools window.
- * Content scripts and other extension pages do not have these APIs.
- * Thus, the APIs are available only through the lifetime of the DevTools window.
+ * https://developer.chrome.com/extensions/devtools
+ * The chrome.devtools.* API modules are available
+ * only to the pages loaded within the DevTools window.
  */
 chrome.devtools.network.onRequestFinished.addListener(request => {
   let searchString = "/java/session";

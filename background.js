fe49967d71ae562abefc00f5af578c9e2ac5961f
Artur Kovalchuk <artur.kovalchuk@worldapp.com>
2020-08-17 20:07:21 +0300

    Polish

--- background.js
+++ background.js
@@ -1,14 +1,10 @@
-'use strict';
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
+      console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
       if (request.greeting === "hello"){
         console.log("body in request.body = " + request.body);
         sendRequest();
-        sendResponse({farewell: "goodbye"});
+        sendResponse({farewell: "ok"});
       }
     });
 
--- devtools.js
+++ devtools.js
@@ -1,1 +1,17 @@
-chrome.devtools.panels.create("MyPanel24", null, 'panel.html');
+/**
+ * https://stackoverflow.com/questions/29845765/chrome-extension-using-devtool-for-har-file-save
+ * The chrome.devtools.* API modules
+ * are available only to the pages loaded within the DevTools window.
+ * Content scripts and other extension pages do not have these APIs.
+ * Thus, the APIs are available only through the lifetime of the DevTools window.
+ */
+chrome.devtools.network.onRequestFinished.addListener(request => {
+  let searchString = "/java/session";
+  if ( request.request.url.endsWith(searchString)) {
+    request.getContent((body) => {
+      chrome.runtime.sendMessage({greeting: "hello", body: body}, function(response) {
+        console.log("Request url = " + request.request.url + ", body = " + body + ", response = " + response.farewell);
+      });
+    });
+  }
+});
